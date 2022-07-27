70fadf657c30cdace8645e632f0cb6a495c9f639
pilicercato <mpilicercato@gmail.com>
2022-07-27 11:49:36 -0300

    first commit

---
+++ src/test/java/com/cercato/msconfigserver/MsConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cercato.msconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
